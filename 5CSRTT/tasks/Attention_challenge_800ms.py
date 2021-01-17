51979b37a3ebabc749df49d4b0cdce36a87d7db2
KaetzelLab <71041273+KaetzelLab@users.noreply.github.com>
2021-01-17 12:28:54 +0100

    Update Attention_challenge_800ms.py

--- 5CSRTT/tasks/Attention_challenge_800ms.py
+++ 5CSRTT/tasks/Attention_challenge_800ms.py
@@ -1,31 +1,35 @@
-"""Task file is for 5-CSRTT- task starts with reward and trial starts by presenting one of the five-choice
-light_on for a defined stimulus duration (state_dur)+ extra time (limited hold where cue light turned off, and mouse
-still get chance to make choice) correct poke leads to reward then iti or incorrect and omission leads to time out
+#--------------------------5-CSRTT attention challege 2 Protocol Task File----------------------
 
-Correct response ==> reward ==> iti ==> cue presentation
+# cue presentation ==> Correct response       ==> reward(@reward port)      ==> iti** ==> cue presentation
+# cue presentation ==> Incorrect response     ==> penalty (house light off) ==> iti** ==> cue presentation
+# cue presentation ==> omission (no response) ==> penalty (house light off) ==> iti** ==> cue presentation
+# **any response at 5-Poke during the iti is considered as premature response and trial starts again after penalty(house light off)
 
-Incorrect response ==> penalty (house light off) ==> iti ==> cue presentation
+# Important readouts:
+# Responses :- number of Corrects, number of Incorrects, number of Omissions, number of Prematures
+# Latencies :-Reward lat., Premature lat., Correct lat.,
 
-omission (no response) ==> penalty (house light off) ==> iti ==> cue presentation
-Important readouts:
-Responses :- #Corrects, #Incorrects, #Omissions, #Prematures
-Latencies :-Reward lat., Premature lat., Correct lat.,
-"""
+# important note:- if reward port SOL not used and intended to use 'house_light = Digital_output(pin=board.port_5.POW_A)'
+#  ....then this line of code 'hw.reward_port.SOL.on()'  need to modify as 'hw.house_light.on()' to control HouseLight 
+
+#--------------------------------------------------------------------------
+
 
 # necessary imports to begin code
+
 from pyControl.utility import *
 import hardware_definition as hw
 
 
-# list of states
-states = ['start',
-          'choice_task',
-          'reward',
-          'penalty',
-          'iti']
+# list of states and events
 
-# list of events
-events = ['session_timer',
+states = ['start',
+          'choice_task', #Cue Light presentation
+          'reward',      #Reward Statefor correct response
+          'penalty',     #time out HouseLight off
+          'iti']         #inter-trial interval
+
+events = ['session_timer',      # timer to terminate session after defined time (30min usually)
           'poke_1',             # light 1
           'poke_2',             # light 2
           'poke_3',             # light 3
@@ -38,46 +42,47 @@ events = ['session_timer',
           'penalty_omission']   # timer to wait for response for defined period
 
 # initial state name (required by pyControl)
-
 initial_state = 'start'
 
-# variables needed
-
+# variables
 # variables changes according to stages
-v.state_dur = 0.8 * second         # cue stimulus duration
-v.ITI_dur = 5 * second            # inter trial interval duration
+
+v.state_dur = 0.8 * second      # cue stimulus duration
+v.ITI_dur   = 5 * second        # inter trial interval duration
 
 #constant variable for all stages
-v.session_duration = 30 * minute  # Session duration
-v.LH_dur = 2 * second             # limited hold duration
-v.reward_stim_dur = 2 * second    # reward duration this duration
-v.penalty_dur = 5 * second        # penalty duration(All lights off)
-v.steps_rate = 1000               # step rate for stepper motor if you want to use peristaltic pump
-v.n_steps = 1300                  # number of steps for stepper motor if you want to use peristaltic pump
-v.target = 0
+
+v.session_dur   = 30 * minute  # Session duration
+v.LH_dur        = 2 * second   # limited hold duration
+v.reward_in_dur = 2 * second   # reward duration this duration
+v.penalty_dur   = 5 * second   # penalty duration(All lights off)
+v.steps_rate    = 1000         # step rate for stepper motor if you want to use peristaltic pump
+v.n_steps       = 1300         # number of steps for stepper motor if you want to use peristaltic pump
+v.target        = 0
 
 
 # states defines starts and ending of task file
 def run_start():
-    set_timer('session_timer', v.session_duration)
+    set_timer('session_timer', v.session_dur)
     hw.reward_port.SOL.on() # turn on house light - we use reward port SOL to power house light
 
 
 def run_end():
-    hw.off()
+    hw.off() # Turn off the Hardware
     hw.reward_port.SOL.off() # turn off house light - we use reward port SOL to power house light
 
 
 # initial state
+#5-CSRTT always starts by delivering reward
 def start(event):
     if event == 'entry':
-        hw.reward_port.LED.on() # turn on reward port LED on
+        hw.reward_port.LED.on()                           # turn on reward port LED on
         hw.syringe_pump.backward(v.steps_rate, v.n_steps) # beginning reward
     elif event == 'exit':
         hw.reward_port.LED.off()
         disarm_timer('reward_in_timer')
     elif event == 'poke_6':
-        set_timer('reward_in_timer', v.reward_stim_dur) 
+        set_timer('reward_in_timer', v.reward_in_dur) # timer to turn off rewrard stim after speified time(2sec usually) 
     elif event == 'poke_6_out':
         print("ITI_duration:{} \n SD_duration:{}\n LH_duration:{}".format(v.ITI_dur, v.state_dur, v.LH_dur))
         goto_state('choice_task')
@@ -89,9 +94,9 @@ def choice_task(event):
         print('Sample_state')
         set_timer('port_lights_timer', v.state_dur)
         set_timer('penalty_omission', (v.state_dur + v.LH_dur))
-        v.target = random.randint(1, 5)
-        print('v.target:{}'.format(v.target))
-        # randomly select int between 1-5 and variable referred as v.target and present cue light accordingly
+        v.target = random.randint(1, 5) # randomly select int between 1-5 and variable as v.target 
+        print('v.target:{}'.format(v.target))        
+        #present cue light according to v.target
         if v.target == 1:
             hw.five_poke.poke_1.LED.on() # 5-choice hole_1 light on
         elif v.target == 2:
@@ -110,7 +115,7 @@ def choice_task(event):
             or event == 'poke_5' and v.target == 5:
         print('Correct_response') # the latency between cue presentation and correct response is correct lat.
         goto_state('reward')
-    # if event (poke) is other than target , considered as incorrect response and go to penalty state
+    # if event (poke) is other than target, considered as incorrect response and go to penalty state
     elif event == 'poke_1' and v.target != 1 \
             or event == 'poke_2' and v.target != 2 \
             or event == 'poke_3' and v.target != 3 \
@@ -118,15 +123,15 @@ def choice_task(event):
             or event == 'poke_5' and v.target != 5:
         print('Incorrect_response') # the latency between cue presentation and incorrect response is incorrect lat
         goto_state('penalty')
-    # # if event is no respose, considered as omission and go to penalty state
+    # if event ( no respose) is same as target , considered as omission and go to penalty state
     elif event == 'exit':
-        hw.five_poke.poke_1.LED.off() # 5-choice hole_1 light off
-        hw.five_poke.poke_2.LED.off() # 5-choice hole_2 light off
-        hw.five_poke.poke_3.LED.off() # 5-choice hole_3 light off
-        hw.five_poke.poke_4.LED.off() # 5-choice hole_4 light off
-        hw.five_poke.poke_5.LED.off() # 5-choice hole_5 light off
+        hw.five_poke.poke_1.LED.off()     # 5-choice hole_1 light off
+        hw.five_poke.poke_2.LED.off()     # 5-choice hole_2 light off
+        hw.five_poke.poke_3.LED.off()     # 5-choice hole_3 light off
+        hw.five_poke.poke_4.LED.off()     # 5-choice hole_4 light off
+        hw.five_poke.poke_5.LED.off()     # 5-choice hole_5 light off
         disarm_timer('port_lights_timer') # clear timer for 5-Choice port lights
-        disarm_timer('penalty_omission') # clear timer for omission
+        disarm_timer('penalty_omission')  # clear timer for omission
 
 
 # reward state
@@ -139,7 +144,7 @@ def reward(event):
     elif event == 'poke_6':
         hw.syringe_pump.backward(v.steps_rate, v.n_steps) # Reward after correct resonse and poke into reward receptacle
         print('Reward_taken')                             # the latency between correct and Reward_taken is Reward lat.
-        set_timer('reward_in_timer', v.reward_stim_dur)
+        set_timer('reward_in_timer', v.reward_in_dur)
     elif event == 'poke_6_out':
         goto_state('iti')
     elif event == 'poke_1' and v.target == 1 \
@@ -203,3 +208,4 @@ def all_states(event):
         print('5_poke_entries')
     elif event == 'poke_6_out':
         print('receptacle_entries')
+
